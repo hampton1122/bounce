8fa1867a5d0628fd4627bc707298eab09571f700
Chris Hampton <champton@samaritan.org>
2024-08-16 11:30:31 -0400

    added social media icon and font awesome

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const redirectUrl = 'https://www.instagram.com/bouncechildrensco/';;
+    const iconTextElement = document.getElementById('social-link');
+    iconTextElement.addEventListener('click', function() {
+        window.open(redirectUrl, '_blank');
+    });
+  });

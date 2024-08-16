29d4f519ffb6db9512c3f47c65f213ad8460c9c8
Chris Hampton <champton@samaritan.org>
2024-08-16 13:36:10 -0400

    updated js to take to google maps

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
 document.addEventListener('DOMContentLoaded', function() {
-    const redirectUrl = 'https://www.instagram.com/bouncechildrensco/';;
+    const redirectUrl = 'https://www.instagram.com/bouncechildrensco/';
     const iconTextElement = document.getElementById('social-link');
+    iconTextElement.addEventListener('click', function() {
+        window.open(redirectUrl, '_blank');
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const redirectUrl = 'https://maps.app.goo.gl/w4HqKJEMgLkbkxbu5';
+    const iconTextElement = document.getElementById('location-link');
     iconTextElement.addEventListener('click', function() {
         window.open(redirectUrl, '_blank');
     });
